86b9f306a72460975d3a6dd50c945f7b7e9b26b6
hdcurillo <davicurillo1905@gmail.com>
2023-01-25 18:14:42 -0500

    BacKend en el puerto 4447

---
+++ BEComentarios/AplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using BEComentarios.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BEComentarios
+{
+    public class AplicationDBContext: DbContext
+    {
+        public DbSet<Comentario> Comentarios { get; set; }
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ BEComentarios/Models/Comentario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BEComentarios.Models
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Autor { get; set; }
+        [Required]
+        public string Texto { get; set; }
+        [Required]
+        public DateTime FechaCreate { get; set; }
+
+    }
+}
